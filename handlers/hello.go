9e121489cf316ea8ee318b7abe7bbfbc8688aa29
Calleum <calleum.p@protonmail.com>
2020-11-25 17:39:19 +1100

    init

---
+++ handlers/hello.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("Hello World!")
+	d, _ := ioutil.ReadAll(r.Body)
+
+	log.Printf("Data %s\n", d)
+}
